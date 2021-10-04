0b994c7299a1bb944668e28cb4537e6f0c5b7ac2
hipercub0 <73201408+hipercub0@users.noreply.github.com>
2021-10-04 00:05:20 -0300

    Crear logout.html y logout.js

---
+++ js/logout.js
@@ -0,0 +1,12 @@
+function LogOut() {
+    localStorage.removeItem('nombreUsuario');
+    window.location.replace("index.html");
+}
+
+
+//Función que se ejecuta una vez que se haya lanzado el evento de
+//que el documento se encuentra cargado, es decir, se encuentran todos los
+//elementos HTML presentes.
+document.addEventListener("DOMContentLoaded", function (e) {
+    LogOut();
+});
